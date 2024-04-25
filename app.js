5f66a9e664445bcf771560e3a9037672b9242de5
Justin <ju3tin@hotmail.co.uk>
2024-04-25 18:38:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
+const cors = require("cors");
+
 var app = express();
 
 // view engine setup
