cc85c712f14424a08b9c5802082be98eb7eddb67
ju3tin <ju3tin@hotmail.co.uk>
2024-04-25 10:28:49 +0100

    sdsdsd

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,"/shit/index.html"));
+})
+
+
+
+//app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
