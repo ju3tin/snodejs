a27571501fb29d7295cd2e05e1e6cb3b151ce00c
ju3tin <ju3tin@hotmail.co.uk>
2024-04-26 11:41:00 +0100

    dfddd

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const mongoose=require('mongoose');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
+dotenv.config({ path: './config/config.env' })
 const cookieSession = require("cookie-session");
 
 const dbConfig = require("./app/config/db.config");
